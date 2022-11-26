34118ace5a73fa29b13f9343c5997f74f2db6492
Guido <guidocalciano@gmail.com>
2022-11-26 20:32:12 -0300

    project updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+// _app wraps around all page components
+
+import Header from '../components/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <Component {...pageProps} />
+      </main>
+      
+    </>
+  ) 
 }
 
 export default MyApp
---
+++ posts/test.md
@@ -0,0 +1,12 @@
+---
+title: 'Test Post'
+date: 'June 24 2022'
+excerpt: 'This is the excerpt'
+cover_image: '/images/posts/img1.jpg'
+---
+## Heading Two
+
+*Item 1
+*Item 2
+
+Hello World
