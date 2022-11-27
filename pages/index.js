1f3bf89d2f206ff4ebad63c18d6d4cce476578c3
Guido <guidocalciano@gmail.com>
2022-11-26 21:27:31 -0300

    finished project v1

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import path from 'path'
 import matter from 'gray-matter'
 import Head from 'next/head'
 import Post from '../components/Post'
+import {sortByDate} from '../utils'
 
 
 
@@ -48,7 +49,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts: posts
+      posts: posts.sort(sortByDate)
     }
   }
 }
